1ecca21702e0ddcf3b0d0084dfbf8089bbd468ca
Gaurav Negi <gauravnegi2235@gmail.com>
2024-06-14 17:07:16 +0530

    added authentication

--- models/Person.js
+++ models/Person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const personSchema = new mongoose.Schema({
     name:{
         type:String,
@@ -37,6 +38,31 @@ const personSchema = new mongoose.Schema({
         type:String
     }
 })
+// press is callback funcyion
+
+personSchema.pre('save',async function(next){
+    const person = this;
+    if(!person.isModified('password')) return next();
+    try{
+        const salt = await bcrypt.genSalt(10);
+
+        const hashPassword = await bcrypt.hash(person.password, salt);
+        person.password = hashPassword;
+        next();
+    }
+    catch(err){
+        return next(err);
+    }
+})
+
+personSchema.methods.comparePassword = async function(candidatePassword) {
+    try {
+        return await bcrypt.compare(candidatePassword, this.password);
+    } catch (err) {
+        throw new Error('Error comparing password'); // More descriptive error message
+    }
+};
+
 
 //creating model;
 const Person = mongoose.model('Person',personSchema);
