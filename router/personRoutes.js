c26ac6887876990d3ac0883755ece8d05dfdf52e
Gaurav Negi <gauravnegi2235@gmail.com>
2024-06-16 16:54:01 +0530

    added token based auth

--- router/personRoutes.js
+++ router/personRoutes.js
@@ -1,19 +1,70 @@
 const express = require('express');
 const Person = require('./../models/Person');
 const router = express.Router();
-router.post('/',async(req,res)=>{
+const {jwtAuthMiddleware , generateToken} = require('./../jwt')
+router.post('/signup',async(req,res)=>{
     try{
         const data = req.body;
         const newPerson = new Person(data);
         const response = await newPerson.save();
         console.log("Data Saved");
-        res.status(200).json(response);
+
+        // const token = generateToken(response.username);
+        const payload ={
+            id:response.id,
+            username:response.username
+        }
+        console.log(payload)
+        const token = generateToken(payload);
+        console.log("Token is  : " , token);
+        res.status(200).json({response :response , token :token});
     }catch(err){
         console.log(err);
         res.status(500).json({err:"Error while saving"})
     }
 })
-router.get('/',async(req,res)=>{
+
+router.post('/login',async(req,res)=>{
+    try{
+        const {username , password} = req.body;
+
+        const user = await Person.findOne({username: username});
+        if(!user || !(await user.comparePassword(password))){
+            return res.status(401).json({err:"Invalid username or password"});
+        }
+
+        const payload={
+            id:user.id,
+            username:user.username
+        }
+        const token = generateToken(payload);
+        res.json({token})
+    }catch(error){
+        console.log(error);
+        res.status
+    }
+})
+
+router.get('/profile', jwtAuthMiddleware, async (req, res) => {
+    try {
+        const userData = req.user;
+        console.log("User Data:", userData);
+        const userId = userData.id; // Assuming `id` is the correct property in `req.user`
+
+        // Fetch user data from MongoDB
+        const user = await Person.findById(userId);
+        
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        res.status(200).json({ user });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Internal Server Error" });
+    }}
+) 
+router.get('/' ,jwtAuthMiddleware,async(req,res)=>{
     try{
         const data = await Person.find();
         console.log("Data Fetched");
@@ -28,7 +79,7 @@ router.get('/',async(req,res)=>{
 
 ///Params
 
-router.get('/:workType', async(req, res)=>{
+router.get('/:workType',jwtAuthMiddleware, async(req, res)=>{
     try{
         const workType = req.params.workType;
         if(workType=='waiter' || workType=='chef' || workType=='manager'){
